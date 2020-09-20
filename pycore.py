92b925c8d7b2d93f84d21c767a8a963f5f482668
Javier Alcoriza <jalcoriza@gmail.com>
2020-09-20 19:31:39 +0200

    pycore.py as an state machine to process async inputs

--- pycore.py
+++ pycore.py
@@ -4,6 +4,7 @@
 #
 import RPi.GPIO as GPIO
 import time
+import datetime
 
 relay_01 = 4 # BCM4, pin7, OUT-01, bulb light
 relay_02 = 17 # BCM17, pin11, OUT-02, garage door
@@ -13,50 +14,98 @@ relay_05 = 10 # BCM10, pin19, OUT-05
 input_01 = 9 # BCM9, pin21, IN-01, short input
 input_02 = 11 # BCM11, pin23, IN-02, long input
 
-#period = 0.01 # Period in seconds
-period = 60 # Period in seconds
-t = 3600 * 24 # Time in seconds
-#num = int(t/period)
-num = 1
+period = 0.5 # Period in seconds
+t = 0
+hysteresis = False
+pir_count = 0
+pir_time = 60
 
-print('Initializing GPIO...')
-#print 'period =', period,'seconds'
-#print 'time =', t, 'seconds'
-#print 'num =', num, 'times'
+def init_gpio():
+    # Initialize GPIO
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(relay_01, GPIO.OUT) # GPIO assign mode
+    GPIO.setup(relay_02, GPIO.OUT) # GPIO assign mode
+    GPIO.setup(relay_03, GPIO.OUT) # GPIO assign mode
+    GPIO.setup(relay_04, GPIO.OUT) # GPIO assign mode
+    GPIO.setup(relay_05, GPIO.OUT) # GPIO assign mode
+    GPIO.setup(input_01, GPIO.IN, pull_up_down = GPIO.PUD_DOWN) # input with pull-down 
+    GPIO.setup(input_02, GPIO.IN, pull_up_down = GPIO.PUD_DOWN) # input with pull-down 
 
-# Initialize GPIO
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(relay_01, GPIO.OUT) # GPIO assign mode
-GPIO.setup(relay_02, GPIO.OUT) # GPIO assign mode
-GPIO.setup(relay_03, GPIO.OUT) # GPIO assign mode
-GPIO.setup(relay_04, GPIO.OUT) # GPIO assign mode
-GPIO.setup(relay_05, GPIO.OUT) # GPIO assign mode
-GPIO.setup(input_01, GPIO.IN) # GPIO assign mode
-GPIO.setup(input_02, GPIO.IN) # GPIO assign mode
+    return 0
+
+def write_gpio():
+    GPIO.output(relay_01, output_gpio[0])
+    GPIO.output(relay_02, output_gpio[1])
+    GPIO.output(relay_03, output_gpio[2])
+    GPIO.output(relay_04, output_gpio[3])
+    GPIO.output(relay_05, output_gpio[4])
+
+    return 0
+
+def read_gpio():
+    input_gpio[0] = GPIO.input(input_01)
+    input_gpio[1] = GPIO.input(input_02)
+
+    return 0
+
+def status_gpio():
+    print(f'{datetime.datetime.now()} input={input_gpio} output={output_gpio}')
+
+    return 0
+
+def process_pir():
+    global hysteresis
+    global pir_count
+
+    if (hysteresis): 
+        pir_count += 1;
+        if (pir_count * period) > pir_time:
+            hysteresis = False
+            pir_count = 0
+
+    else:
+        if (input_gpio[0]==GPIO.HIGH):
+            output_gpio[0] = GPIO.LOW
+            hysteresis = True
+            print(f'{datetime.datetime.now()} detected movement! turn on the light!')
+
+        else:
+            output_gpio[0] = GPIO.HIGH
+
+
+    return 0
+
+def process_automaton():
+    process_pir()
+
+    return 0
+
 
 # Initialize GPIO outputs to HIGH (it doesn't activate the relays)
-GPIO.output(relay_01, GPIO.HIGH) # output 1
-GPIO.output(relay_02, GPIO.HIGH) # output 1
-GPIO.output(relay_03, GPIO.HIGH) # output 1
-GPIO.output(relay_04, GPIO.HIGH) # output 1
-GPIO.output(relay_05, GPIO.HIGH) # output 1
+# main() code
 
+output_gpio = [GPIO.HIGH, GPIO.HIGH, GPIO.HIGH, GPIO.HIGH, GPIO.HIGH]
+input_gpio = [GPIO.LOW, GPIO.LOW]
 
-print(f'Initialization, waiting {period} seconds...')
-time.sleep(period)
-print('done!')
+print('Initializing GPIO...')
+print(f'period={period}s, time={t}s')
+init_gpio()
+
+print('Initializing relays OUTPUT as HIGH...')
+write_gpio()
 
 try:
     # Infinite loop waiting for a CTRL^C
     while True:
-        # Loop repeats num times
-        for x in range(num):
-            #print 'Set pin',relay_01,'(BCM) HIGH (#',x,')'
-            #GPIO.output(relay_01, GPIO.HIGH)
-            #time.sleep(period/60)
-            print(f'Set pin {relay_01} (BCM) LOW (#{x})')
-            GPIO.output(relay_01, GPIO.LOW)
-            time.sleep(period)
+        #print(f'Set pin {relay_01} (BCM) LOW (t={t * period}s)')
+        #output_gpio[0] = GPIO.LOW
+        process_automaton()
+
+        t += 1
+        write_gpio()
+        read_gpio()
+        status_gpio()
+        time.sleep(period)
 
 except KeyboardInterrupt:
    GPIO.cleanup()
